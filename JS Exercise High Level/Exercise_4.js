d9b4c36fa20dbe9b19903c9199a76868b888060c
MinhBear <hvmnhatminh@gmail.com>
2022-10-26 00:38:20 +0700

    update Exercise_4

--- JS Exercise High Level/Exercise_4.js
+++ JS Exercise High Level/Exercise_4.js
@@ -18,22 +18,28 @@ const controller2 = (req, res) => {
 }
 
 //Giả sử có 3 request đồng thời gọi vào controller1 và controller2 thì thời 
-//gian để nhận được respone tại controller1 và controller2 của request đầu
-//tiên là bao lâu
+//gian để nhận được respone tại controller1 và controller2 của request cuối cùng
+// là bao lâu
 //Đề xuất cải thiện performance cho controller2
 
 //--------------------------------------------------------------------------
 /**
- * Với 3 request đồng thời gọi vào controller1 và controller2 thì thời gian nhận được respone
- * của request cuối cùng tại controller2 là hơn 30s còn controller1 sẽ hơn 60s. 
- *
- * Để cải thiện controller 2 thì nên chuyển cơ chế sang non blocking, thay vì dùng while để chời 10s thì mình dùng
- * setTimeout như thế cả 2 controller sẽ thực thi dưới dạng non blocking và respone của các controller về với các request là như nhau
- * mà không phải chờ waitblocking chạy xong rồi mới chạy non blocking
+ * request cuối cùng nhận được controller1 hơn 10s và controller2 hơn 30s
+ * Khi cả 3 request gọi đồng thời đến 2 controller, controller1 sự dụng cơ chế non blocking và thực hiện hàm setTimeout trong 
+ * với một biến Timer được đặt sắn để keep track thời gian của hàm nonBlocking, tương tự với nonBlocking của 2 request còn lại gọi đến và đều có 
+ * một biến Timer keep track thời gian wait và sau khi wait xong add cả 3 vào taskQueue theo thứ tự từ request 1 đến request cuối
+ * 
+ * Với controller2 thì dùng cơ chế blocking mà node js chỉ có một theared để xử lý các request cho nên request cuối cùng phải đợi 2 request đầu tiên
+ * nhận được respone rồi mới được thực hiện hàm waitBlocking nên thời gian mất là hơn 30s
  */
 
-//FIX waitBlocking
-const controller2_FIX = (req, res) => {
-    setTimeout(waitBlocking(0), 10000);
-    res.status(200).end();
+//Cải tiến controller 2
+/**
+ * Với controller 2 ta nên chuyển cơ chế của hàm waitBlocking sang non Blocking bằng cách dùng setTimeout
+ */
+
+ const waitBlocking = (milisecond) => {
+    setTimeout(() => {
+        //do something
+    }, milisecond);
 }
